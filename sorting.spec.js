76fe7b0e9c4d99198da147187b679b82ebd9f157
Cristina <cristina.colon+git@gmail.com>
2015-07-31 11:33:01 -0400

    start on call count spec for merge sort

--- sorting.spec.js
+++ sorting.spec.js
@@ -13,3 +13,38 @@ describe('Bubble Sort', function(){
     });
 
 });
+
+
+describe('Merge Sort', function(){
+    it('handles an empty array', function(){
+        expect( mergeSort([]) ).toEqual( [] );
+    });
+    it('takes an array of numbers, and returns a correctly sorted array', function(){
+        expect( mergeSort([8,7,6,5,4,3,2,1]) ).toEqual( [1,2,3,4,5,6,7,8] );
+    });
+    it('takes an array of strings, and returns a correctly sorted array', function(){
+        expect( mergeSort(['c','a','b','e','d']) ).toEqual( ['a','b','c','d','e'] );
+    });
+    it('handles an already sorted array', function(){
+        expect( mergeSort([2,3,4,7,8]) ).toEqual( [2,3,4,7,8] );
+    });
+    it('calls itself once on an empty array', function() {
+      spyOn(window, 'mergeSort').and.callThrough();
+      var arr = [];
+      mergeSort(arr);
+      expect(mergeSort.calls.count()).toEqual(1);
+    });
+    it('calls itself once on an array with one element', function() {
+      spyOn(window, 'mergeSort').and.callThrough();
+      var arr = [1];
+      mergeSort(arr);
+      expect(mergeSort.calls.count()).toEqual(1);
+    });
+    /*
+    it('calls itself n + 1 times', function() {
+      spyOn(window, 'mergeSort').and.callThrough();
+      var arr = [5,4,3,2,1];
+      mergeSort(arr);
+      expect(mergeSort.calls.count()).toEqual();
+    });*/
+});
